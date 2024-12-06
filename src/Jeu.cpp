a27dceab730ce8229e1c8e0e800c665dbae61a60
Moyakeko <kevinyannh@gmail.com>
2024-12-06 14:17:34 +0100

    maj tout court

--- src/Jeu.cpp
+++ src/Jeu.cpp
@@ -7,10 +7,36 @@ void Jeu::Save(){
 
 }
 void Jeu::Load(){
-  std::vector <StandardCellule> v;
-}
-void Jeu::Launch(){
+  //std::vector <StandardCellule> v;
+  //void Jeu::Load() {
+    std::ifstream fichier("grille.txt"); // Chemin du fichier à charger
+    if (!fichier.is_open()) {
+      std::cerr << "Erreur : Impossible d'ouvrir le fichier." << std::endl;
+      return;
+    }
 
+    int lignes, colonnes;
+    fichier >> lignes >> colonnes; // Lecture des dimensions de la grille
+    std::vector<int> Cellules
+
+    for (int i = 0; i < lignes; ++i) {
+      for (int j = 0; j < colonnes; ++j) {
+        bool etat;
+        fichier >> etat; // Lire l'état de chaque cellule (0 ou 1)
+        Cellules.push_back(etat);
+      }
+    }
+
+    fichier.close();
+
+    GrilleClassique Grille(colonnes, lignes, Cellules);
+
+
+}
+
+
+void Jeu::Launch(){
+  Grille.afficherGrille();
 }
 
 
