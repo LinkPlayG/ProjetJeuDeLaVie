d67f86bd71de85aa8701cbd81025a5c7647c1e42
Link_PlayG <lucastocker241006@gmail.com>
2024-12-06 08:37:19 +0100

    Fused classes in a single file

--- hea/AbstractCellule.h
+++ hea/AbstractCellule.h
@@ -2,8 +2,8 @@
 // Created by lucas on 03/12/2024.
 //
 
-#ifndef ABSTRACTCELLULE_H
-#define ABSTRACTCELLULE_H
+#ifndef CELLULE_H
+#define CELLULE_H
 
 
 class AbstractCellule{
@@ -16,7 +16,26 @@ public:
     AbstractCellule(int x, int y, int state);
     virtual int getX() const;
     virtual int getY() const;
-    virtual int GetState();
+    virtual int GetState()const;
 };
 
-#endif //ABSTRACTCELLULE_H
+class StandardCellule: public AbstractCellule{
+public:
+    StandardCellule();
+    StandardCellule(int x, int y, int state);
+    int getX() const override;
+    int getY() const override;
+    int GetState() const override;
+    void ChangeState(int s);
+};
+
+class Obstacle:public AbstractCellule{
+public:
+    Obstacle();
+    Obstacle(int x,int y, int state);
+    int getX()const override;
+    int getY()const override;
+    int GetState()const override;
+};
+
+#endif //CELLULE_H
